e99311173957193f20db165df2540229d3eaa6a8
lilly <lilly1987@naver.com>
2023-03-19 23:02:29 +0900

    Update random_loop.py

--- random_loop.py
+++ random_loop.py
@@ -1,19 +1,38 @@
+import os, glob, sys
 import json
 from urllib import request, parse
 import random
-import os, glob
+import wildcards
 
 prompt={}
 names={}
+ckpts=glob.glob(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..\\models\\checkpoints"
+    )+"\\*-fp16.safetensors"
+)
+ckptnms=[os.path.basename(ckpt) for ckpt in ckpts]
 
+sys.exit()
+
+def lget(a):
+    return random.choice(a) if type(a) is list else a
 
 def pget(name,input):
     return prompt[names[name]]["inputs"][input]
     
 def pset(name,input,value):
-    prompt[names[name]]["inputs"][input] =  value
-    
+    prompt[names[name]]["inputs"][input] = lget(value)
 
+def psetd(name,kv):
+    for k, v in kv.items():
+        pset(name,k,v)
+
+def cget(c,v,t):
+    p=chars[c][v] if v in chars[c] else t
+    return lget(p)
+    
 def prompt_add(name,class_type,inputs):
     n=f"{len(names)}"
     names[name]=n
@@ -29,14 +48,6 @@ def queue_prompt(prompt):
     req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
     request.urlopen(req)
     
-ckpts=glob.glob(
-    os.path.join(
-        os.path.dirname(__file__),
-        "..\\models\\checkpoints"
-    )+"\\*-fp16.safetensors"
-)
-
-ckptnms=[os.path.basename(ckpt) for ckpt in ckpts]
 
 #print(f"ckpts {ckptnms}")
 
@@ -149,33 +160,45 @@ prompt_add(
 #print(names)
 #print(prompt)
 
+shoulder="off shoulder, bare shoulders, Strapless,"
 quality="masterpiece, best quality, clear details, detailed beautiful face, ultra-detailed,"
-dress="{sweater|maid|princess royal|santa|lolita fashion|china|witch|wedding|yukata|kimono|} {frilled |}{long |}dress, {{puffy | }{wide |}{long |}sleeves,|} high heels, off shoulder, bare shoulders, Strapless,"
-NSFW="NSFW, breasts exposure, breastsout, nipple exposure, "
+dress="{sweater|maid|princess royal|santa|lolita fashion|china|witch|wedding|yukata|kimono|} {frilled |}{long |}dress, {{puffy | }{wide |}{long |}sleeves,|} high heels, "+shoulder
+NSFW=["NSFW, breasts exposure, breastsout, nipple exposure, "]
+prompt_my="long hair, sharp eyes, sharply eyelashes, sharply eyeliner, small breasts,"
 
 chars={ 
     "my" : {
-        "prompt" : "long hair, sharp eyes, sharply eyelashes, sharply eyeliner, small breasts,",
+        "prompt" : prompt_my,
+        #"strength_model" : [0.5,1.0]
     },
     "diana" : {
         "prompt" : "diana cavendish, long wavy hair, multicolored two-tone streaked hair, light green hair, light blonde hair, {sharp eyes, sharply eyelashes, sharply eyeliner,| } small breasts,",
-        "lora" : "dianaCavendishLittle_v11ClothesFix.safetensors"
+        "lora" : "dianaCavendishLittle_v11ClothesFix.safetensors",
+        
     },
     "schnee" : {
         "prompt" : "weiss schnee, white long hair, bangs, hair between eye, {sharp eyes, sharply eyelashes, sharply eyeliner,| } small breasts,",
-        "lora" : ["weissSchneeRWBY_weissSchneeV10.safetensors","weissSchneeLORA_weissSchnee.safetensors"]
+        "lora" : ["weissSchneeRWBY_weissSchneeV10.safetensors","weissSchneeLORA_weissSchnee.safetensors"],
     },
     "Tomoyo" : {
         "prompt" : "(daidouji_tomoyo:1.2), (tomoyo:1.2), black long hair, blunt bangs, small breasts, cardcaptor sakura \(style\),",
+        #"dress" : [ "" ,dress],
         "lora" : ["daidoujiTomoyo_v01.safetensors","tomoyo_V1Epoch6.safetensors","sakuraKinomoto_sakuraV1Epoch6.safetensors","cardcaptorSakura_sakuraEpoch6.safetensors"],
-        "strength_model" : [0.5,1.0]
+    },
+    "SaegusaMayumi" : {
+        "prompt" : "mayumi,",
+        "dress" : [ "mahouka_uniformm, green_jacket, see-through lace white long sleeveless dress, shoulder, black high heels, black_pantyhose, " ,dress],
+        "lora" : ["SaegusaMayumiTheIrregularAt_mayumi"],
     },
 
 }
 
 myckpts=["AOM3A1-fp16","libmix_v20-fp16"]
 
-for ckptnm in random.sample(ckptnms,100)+myckpts:
+random.shuffle(ckptnms)
+
+#for ckptnm in random.sample(ckptnms,100)+myckpts:
+for ckptnm in ckptnms:
 
     print(f"ckptnm : {ckptnm}")
     pset("CheckpointLoaderSimple","ckpt_name",ckptnm)
@@ -187,39 +210,42 @@ for ckptnm in random.sample(ckptnms,100)+myckpts:
         for j in range(4):
             
             print(f"char : {c}" )
-            if "prompt" in chars[c]: 
-                #print("prompt" , chars[c]["prompt"])
-                prompt[names["CLIPTextEncodeP"]]["inputs"]["text"] = quality+chars[c]["prompt"]+dress+NSFW
-            else:
-                #print(f"prompt no {c}")
-                prompt[names["CLIPTextEncodeP"]]["inputs"]["text"] = quality+dress+NSFW
+            
+            tmp=""
+            tmp+=cget(c,"quality",quality)
+            tmp+=cget(c,"prompt",prompt_my)
+            tmp+=cget(c,"dress",dress)
+            tmp+=cget(c,"NSFW",NSFW)
+            pset("CLIPTextEncodeP","text", tmp)
                 
             if "lora" in chars[c]: 
                 
-                if type(chars[c]["lora"]) is list :
-                    prompt[names["LoraLoaderTextRandom"]]["inputs"]["lora_name"] = random.choice(chars[c]["lora"])
-                else:
-                    prompt[names["LoraLoaderTextRandom"]]["inputs"]["lora_name"] = chars[c]["lora"]
+                pset("LoraLoaderTextRandom","lora_name",lget(chars[c]["lora"]))
                     
-                prompt[names["LoraLoaderTextRandom"]]["inputs"]["seed"] =  random.randint(0, 0xffffffffffffffff )
+                pset("LoraLoaderTextRandom","seed", random.randint(0, 0xffffffffffffffff ))
                 #if "strength_model" in chars[c]: 
                 #    prompt[names["LoraLoaderTextRandom"]]["inputs"]["strength_model"] =  random.uniform(chars[c]["strength_model"][0],chars[c]["strength_model"][1]) 
                 #else:
                 #    prompt[names["LoraLoaderTextRandom"]]["inputs"]["strength_model"] =  random.uniform(0.25,1.0) 
 
-                prompt[names["KSampler"]]["inputs"]["model"] =  [names["LoraLoaderTextRandom"],0]
-                prompt[names["CLIPTextEncodeP"]]["inputs"]["clip"] =  [names["LoraLoaderTextRandom"],1]
-                prompt[names["CLIPTextEncodeN"]]["inputs"]["clip"] =  [names["LoraLoaderTextRandom"],1]
+                pset("KSampler","model", [names["LoraLoaderTextRandom"],0]   )
+                pset("CLIPTextEncodeP","clip", [names["LoraLoaderTextRandom"],1] )
+                pset("CLIPTextEncodeN","clip", [names["LoraLoaderTextRandom"],1] )
             else:
                 #print(f"lora no {c}")
-                prompt[names["KSampler"]]["inputs"]["model"] =  [names["CheckpointLoaderSimple"],0]
-                prompt[names["CLIPTextEncodeP"]]["inputs"]["clip"] =  [names["CheckpointLoaderSimple"],1]
-                prompt[names["CLIPTextEncodeN"]]["inputs"]["clip"] =  [names["CheckpointLoaderSimple"],1]
+                pset("KSampler","model", [names["CheckpointLoaderSimple"],0] )
+                pset("CLIPTextEncodeP","clip",[names["CheckpointLoaderSimple"],1] )
+                pset("CLIPTextEncodeN","clip",[names["CheckpointLoaderSimple"],1] )
                 
-            prompt[names["KSampler"]]["inputs"]["seed"] =  random.randint(0, 0xffffffffffffffff )
-            prompt[names["KSampler"]]["inputs"]["steps"] =  random.randint(20, 30 )
-            prompt[names["KSampler"]]["inputs"]["cfg"] =  random.randint( int(5*2) , int(9*2) ) / 2
-            prompt[names["KSampler"]]["inputs"]["denoise"] =  random.uniform(0.75,1.0) 
+                psetd(
+                    "KSampler",
+                    {
+                        "seed":random.randint(0, 0xffffffffffffffff ),
+                        "steps":random.randint(20, 30 ),
+                        "cfg":random.randint(int(5*2) , int(9*2) ) / 2,
+                        "denoise":random.uniform(0.75,1.0) ,
+                    }
+                )
             
             #print(prompt)
             
