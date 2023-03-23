7f464f898a957d2276928be77bd9afb29fd9bc3b
lilly <lilly1987@naver.com>
2023-03-24 06:28:51 +0900

    json split

--- mypath.py
+++ mypath.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import ast
 import os
 if __name__ == os.path.splitext(os.path.basename(__file__))[0] :
     from ConsoleColor import print, console
@@ -49,21 +50,48 @@ mainfolder = os.path.dirname(mainFile)
 print("os.path.dirname(mainFile) : " + mainfolder , style="bold CYAN")
 
 def jsondic(full,dic,update=False):
-    
+    #print("full : ",os.path.exists(full),full,style="reset")
+    #print("dic0 : ",dic,style="reset")
     path=os.path.split(full)[0]
+    #print("path : ",path,style="reset")
     if not os.path.exists(path):
         os.makedirs(path)
     #with open(f"./RandomLoop/chars-{time.strftime('_%Y%m%d_%H%M%S')}.json", 'w', encoding='utf-8') as file:
     if os.path.exists(full):
+        #print("dic1 : ",dic,style="reset")
         with open(full, 'r', encoding='utf-8') as file:
-            tmp=json.load(file)
+        
+            text=""
+            lines = file.readlines()
+            for line in lines:
+                line = line.strip()  # 줄 끝의 줄 바꿈 문자를 제거한다.
+                if not line.startswith("#"):
+                    #print("line" , line)
+                    text+=line+"\n"
+            #print("text" , text)
+            text=ast.literal_eval(text)
+            #print("text" , text)
+            text = json.dumps(text)
+            #print("text" , text)
+            tmp=json.loads(text)
+            #print("type(tmp) : ",type(tmp),style="reset")
+            #print("tmp : ",tmp,style="reset")
             if update:
-                dic.update(tmp)
+                if type(dic) is dict:
+                    dic.update(tmp)
+                    #print("dic2 : ",dic,style="reset")
+                elif type(dic) is list:
+                    dic+=tmp
+                    #print("dic3 : ",dic,style="reset")
+                else:
+                    print("type(dic) : ",type(dic),style="reset")
             else:
-                dic=(tmp)
+                dic=tmp
+                #print("dic4 : ",dic,style="reset")
+            #print("dic5 : ",dic,style="reset")
     else:
         with open(full, 'w', encoding='utf-8') as file:
             json.dump(dic, file, sort_keys=False, indent=4)
-            
+    #print("dic6 : ",dic,style="reset")
     return path
         
